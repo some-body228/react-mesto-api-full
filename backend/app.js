7f11f7fa8b56eba7dfea59f1ea2171d5bff9a5b1
KA3AX <Some-body228@yandex.ru>
2021-01-30 00:53:52 +0300

    ref

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ const options = {
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   preflightContinue: false,
   optionsSuccessStatus: 204,
-  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'origin', 'authorization'],
   credentials: true,}
 
 const { PORT = 3000 } = process.env;
