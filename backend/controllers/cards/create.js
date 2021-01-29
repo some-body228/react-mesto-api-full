7c899bbed4a83ac6c7ecd4085cdcd5da706e8422
KA3AX <Some-body228@yandex.ru>
2021-01-30 02:28:57 +0300

    ref

--- backend/controllers/cards/create.js
+++ backend/controllers/cards/create.js
@@ -1,11 +1,16 @@
 const Card = require("../../models/cards");
+const User = require("../../models/users");
 const BadRequestError = require("../../errors/BadRequestError")
 const cardCreate = (req, res, next) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
     .then((card) => {
-      card.owner._id =card.owner
-      res.send(card);
+      User.findById(card.owner)
+        .then(user=>{
+          console.log(user)
+          card.owner = user
+          res.send(card);
+        })
     })
     .catch((err) => {
       if (err.name == "ValidationError") {
