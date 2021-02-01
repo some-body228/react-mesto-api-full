eed4415ba18251d9e04048c882b53d77cc5881ab
KA3AX <Some-body228@yandex.ru>
2021-02-01 20:17:13 +0300

    ref

--- backend/middleware/erorr.js
+++ backend/middleware/erorr.js
@@ -1,4 +1,5 @@
-const errors = (err, req, res) => {
+// eslint-disable-next-line no-unused-vars
+const errors = (err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res.status(statusCode).send({ message: statusCode === 500 ? err.message : message });
 };
